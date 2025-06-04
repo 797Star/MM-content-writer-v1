1bc9edddbd56e8a2875bcbce1f3d2c611b3e7cdf
797Star <88031320+797Star@users.noreply.github.com>
2025-06-04 23:43:30 +0630

    Create app.jsx

---
+++ Fontend/app/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// If you plan to have App-specific styles, you might create an App.css and uncomment:
+// import './App.css'; 
+
+function App() {
+  return (
+    <div>
+      <h1>Burmese Content Generator</h1>
+      <p>Frontend application shell loaded.</p>
+      {/* The content generation form component will be added here later */}
+    </div>
+  );
+}
+
+export default App;
