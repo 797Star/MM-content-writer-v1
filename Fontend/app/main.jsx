c7153b72bc3674df6a6ac8dd77fa062c427dd3f7
797Star <88031320+797Star@users.noreply.github.com>
2025-06-04 23:42:36 +0630

    Create main.jsx

---
+++ Fontend/app/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+// If you plan to add global CSS, you might create an index.css and uncomment:
+// import './index.css' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
